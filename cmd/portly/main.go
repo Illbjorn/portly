1615be048107aaffa381e85740095633ff15493e
Illbjorn <am@hades.so>
2024-10-25 05:36:41 -0400

    Remove Lazy Perf Tracking

--- cmd/portly/main.go
+++ cmd/portly/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/illbjorn/portly/internal/cli"
 )
 
 func main() {
-	// Measure runtime.
-	start := time.Now()
 	cli.Run(os.Args)
-	runtime := time.Since(start)
-	println("---")
-	println("Runtime : " + runtime.String())
-	println("---")
 }
