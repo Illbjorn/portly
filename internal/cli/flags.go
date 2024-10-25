9e9acf8a49667f7c78ca1c9ccc3519d653e784f2
Illbjorn <am@hades.so>
2024-10-25 05:55:52 -0400

    Reimplement Flags as a FlagSet

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -18,50 +18,51 @@ type flags struct {
 }
 
 func parseFlags() flags {
+	fs := flag.NewFlagSet("", flag.ExitOnError)
 	f := flags{}
 
 	// --target, -t
 	// Target to scan.
-	target := flag.String("target", "", "--target [target]")
-	flag.StringVar(target, "t", "", "-t [target]")
+	target := fs.String("target", "", "--target [target]")
+	fs.StringVar(target, "t", "", "-t [target]")
 
 	// --ports, -p
 	// Ports to scan.
-	ports := flag.String("ports", "", "--ports [ports]")
-	flag.StringVar(ports, "p", "", "-p [ports]")
+	ports := fs.String("ports", "", "--ports [ports]")
+	fs.StringVar(ports, "p", "", "-p [ports]")
 
 	// --timeout, -to
 	// Time to wait, per-port per-host, before considering it "closed".
-	timeout := flag.Int("timeout", 1000, "--timeout [num]")
-	flag.IntVar(timeout, "to", 1000, "-to [num]")
+	timeout := fs.Duration("timeout", 1000*time.Millisecond, "--timeout [duration]")
+	fs.DurationVar(timeout, "to", 1000*time.Millisecond, "-to [duration]")
 
 	// --json, -j
 	// Serialize the results as JSON and write to disk.
-	asJSON := flag.String("json", "", "--json [path]")
-	flag.StringVar(asJSON, "j", "", "-j")
+	asJSON := fs.String("json", "", "--json [path]")
+	fs.StringVar(asJSON, "j", "", "-j")
 
 	// --yaml, -y
 	// Serialize the results as YAML and write to disk.
-	asYAML := flag.String("yaml", "", "--yaml [path]")
-	flag.StringVar(asYAML, "y", "", "-y [path]")
+	asYAML := fs.String("yaml", "", "--yaml [path]")
+	fs.StringVar(asYAML, "y", "", "-y [path]")
 
 	// --csv, -c
 	// Serialize the results as CSV and write to disk.
-	asCSV := flag.String("csv", "", "--csv [path]")
-	flag.StringVar(asCSV, "c", "", "-c [path]")
+	asCSV := fs.String("csv", "", "--csv [path]")
+	fs.StringVar(asCSV, "c", "", "-c [path]")
 
 	// --parallel-hosts, -ph
 	// Number of hosts to scan in parallel.
-	parallelHosts := flag.Int("parallel-hosts", 254, "--parallel-hosts [num]")
-	flag.IntVar(parallelHosts, "ph", 254, "-ph [num]")
+	parallelHosts := fs.Int("parallel-hosts", 254, "--parallel-hosts [num]")
+	fs.IntVar(parallelHosts, "ph", 254, "-ph [num]")
 
 	// --parallel-ports, -pp
 	// Number of ports per-host to scan in parallel.
-	parallelPorts := flag.Int("parallel-ports", 8, "--parallel-ports [num]")
-	flag.IntVar(parallelPorts, "pp", 8, "-pp [num]")
+	parallelPorts := fs.Int("parallel-ports", 8, "--parallel-ports [num]")
+	fs.IntVar(parallelPorts, "pp", 8, "-pp [num]")
 
 	// Parse flags.
-	flag.Parse()
+	fs.Parse(os.Args[1:])
 
 	// Assign flag values.
 	f.Target = *target
